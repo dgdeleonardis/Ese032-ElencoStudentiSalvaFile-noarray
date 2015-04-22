95540bbeb19b282c6b591fc98711e9658ad00814
dgdeleonardis <diegowazzaap@hotmail.it>
2015-04-22 18:08:30 +0200

    Commit

--- main.c
+++ main.c
@@ -3,17 +3,44 @@
 #include <string.h>
 
 // Definisci le costanti
-
+#define MAX_STRLEN 30
+#define N 3
 // Definisci il tipo di dato struct s_studente
-
+struct s_studente {
+    char nome[MAX_STRLEN];
+    char cognome[MAX_STRLEN];
+    int eta;
+    char classe[MAX_STRLEN];
+};
 // Definisci studente come struct s_studente
-
+struct s_studente studente;
 
 int main(int argc, char** argv) {
     // Dichiara le variabili locali
+    int i;
     
-    // Codice per l'inserimento, da parte dell'utente, dei dati e del salvataggio
-    // nel file
+    FILE *puntaFile;
+    puntaFile = fopen("elenco.dat", "wb");
+    
+    if(puntaFile != NULL) {
+        for(i = 0; i < N; i++) {
+            printf("Nome elemento %d : ");
+            scanf("%s", studente.nome);
+            printf("Congome elemento %d : ");
+            scanf("%s", studente.cognome);
+            printf("Eta' elemento %d : ");
+            scanf("%d", &(studente.eta));
+            printf("Classe elemento %d : ");
+            scanf("%s", studente.classe);
+            fwrite(&studente, sizeof(studente), 1, puntaFile);
+        }
+        
+        fclose(puntaFile);
+    }
+    
+    else {
+        printf("Il file non puo' essere aperto\n");
+    }
     
     return (EXIT_SUCCESS);
 }
